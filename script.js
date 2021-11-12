bdcf1fb4edbbe190aa55db7a76f4557cf834a7bc
Sergey Kovalchuk <sergey.kovalchuk.utd@gmail.com>
2021-11-12 19:12:33 +0200

    try 1. with the columns

---
+++ script.js
@@ -0,0 +1,13 @@
+(function() {
+    const hexagonsWrapper = document.querySelector(".js-hexagons"),
+          hexagonsColumns = document.querySelectorAll(".js-hexagon-column"),
+          hexagons        = hexagonsWrapper.querySelectorAll(".js-hexagon");
+
+
+        window.addEventListener('resize', function(event) {
+            if(hexagonsWrapper.offsetWidth > window.offsetWidth) {
+                
+            }
+        });
+
+}());
