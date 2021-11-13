0cb6e6cd26835b438bbb6d51bac186f782a80336
Sergey Kovalchuk <sergey.kovalchuk.utd@gmail.com>
2021-11-13 19:56:29 +0200

    removed hexagons columns. need to remove large marggin between hexagons

--- script.js
+++ script.js
@@ -3,19 +3,25 @@
           hexagons        = document.querySelectorAll(".js-hexagon");
 
         const hexaGrid = () => {
+            let hexagonsLineWidth  = hexagonsWrapper.offsetWidth / (hexagons[0].offsetWidth * 0.75),
+                hexagonsLine       = 1,
+                hexagonsLineNumber = 0;
+            
             hexagons.forEach((item, index) => {
-                if(index%2 == 0) {
+                if(index > hexagonsLine + hexagonsLineWidth){
+                    hexagonsLine++;
+                }
+
+                hexagonsLineNumber = index+1 - (hexagonsLine - 1) * hexagonsLineWidth;
+                
+                if(hexagonsLineNumber%2 == 0) {
                     item.classList.add("hexagon--toped");
                 }
-                console.log(index);
-                console.log(item);
             });
         };
-        hexaGrid();
+        hexaGrid();        
         
-        
-          window.addEventListener('resize', (event) => {
-            
+        window.addEventListener('resize', (event) => {
+            hexaGrid();
         });
-
 }());
