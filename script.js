4892b5997d020ff4c33d686ee406b757abd63cab
Sergey Kovalchuk <sergey.kovalchuk.utd@gmail.com>
2021-11-14 10:59:21 +0200

    need to fix wrong class add

--- script.js
+++ script.js
@@ -5,29 +5,25 @@
         const hexaGrid = () => {
             let hexagonsLineWidth  = Math.floor(hexagonsWrapper.offsetWidth / (hexagons[0].offsetWidth * 0.75)),
                 hexagonsLine       = 1;
+                
             
             hexagons.forEach((item, index) => {
                 item.classList.remove("hexagon--toped");
 
-                if(index + 1 > hexagonsLine * hexagonsLineWidth){
+                if(index > hexagonsLine * hexagonsLineWidth){
                     hexagonsLine++;
                 }
 
                 console.log(hexagonsLine);
                 console.log(hexagonsLineWidth);
-                // console.log(1 % 2);
-                // console.log(0 % 2);
-                // console.log(-1 % 2);
+                console.log(1 % 2);
+                console.log(0 % 2);
+                console.log(-1 % 2);
 
-                let hexagonsLineNumber = index - (hexagonsLine - 1) * hexagonsLineWidth;
+                let hexagonsLineNumber = index + 1 - (hexagonsLine - 1) * hexagonsLineWidth;
 
                 if(hexagonsLineNumber % 2 == 0) {
                     item.classList.add("hexagon--toped");
-                } 
-
-                if(hexagonsLine >= 2 && index > hexagonsLineWidth) {
-                    item.classList.add("hexagon--sticked");
-                    item.classList.remove("hexagon--toped");
                 }
             });
         };
